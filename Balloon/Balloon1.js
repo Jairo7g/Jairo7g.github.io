558b706c0d5ef664e3a858d749b931cd5b690caa
Jairo7g <Jairo.gise07@gmail.com>
2025-07-25 10:43:19 -0600

    Update Balloon1.js

--- Balloon/Balloon1.js
+++ Balloon/Balloon1.js
@@ -14,25 +14,25 @@ export default class Balloon1 extends Sprite {
     super(...args);
 
     this.costumes = [
-      new Costume("balloon1-a", "./Balloon1/costumes/balloon1-a.svg", {
+      new Costume("balloon1-a", "./Balloon/costumes/balloon1-a.svg", {
         x: 32,
         y: 94,
       }),
-      new Costume("balloon1-a2", "./Balloon1/costumes/balloon1-a2.svg", {
+      new Costume("balloon1-a2", "./Balloon/costumes/balloon1-a2.svg", {
         x: 29.728883761554442,
         y: 96.65656095717868,
       }),
-      new Costume("balloon1-b", "./Balloon1/costumes/balloon1-b.svg", {
+      new Costume("balloon1-b", "./Balloon/costumes/balloon1-b.svg", {
         x: 31,
         y: 94,
       }),
-      new Costume("balloon1-c", "./Balloon1/costumes/balloon1-c.svg", {
+      new Costume("balloon1-c", "./Balloon/costumes/balloon1-c.svg", {
         x: 31,
         y: 94,
       }),
     ];
 
-    this.sounds = [new Sound("Pop", "./Balloon1/sounds/Pop.wav")];
+    this.sounds = [new Sound("Pop", "./Balloon/sounds/Pop.wav")];
 
     this.triggers = [
       new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked),
