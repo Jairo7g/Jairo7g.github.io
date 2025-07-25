5e9616c7b269adf5d247228373d97cfb0bdfd6dc
Jairo7g <Jairo.gise07@gmail.com>
2025-07-25 10:58:25 -0600

    Update Sprite2.js

--- sprite2/Sprite2.js
+++ sprite2/Sprite2.js
@@ -14,17 +14,17 @@ export default class Sprite2 extends Sprite {
     super(...args);
 
     this.costumes = [
-      new Costume("costume1", "./Sprite2/costumes/costume1.svg", {
+      new Costume("costume1", "./sprite2/costumes/costume1.svg", {
         x: -41.18799999999999,
         y: -19.42641991674938,
       }),
-      new Costume("costume2", "./Sprite2/costumes/costume2.svg", {
+      new Costume("costume2", "./sprite2/costumes/costume2.svg", {
         x: 13.68003631721001,
         y: -19.426419833498784,
       }),
     ];
 
-    this.sounds = [new Sound("pop", "./Sprite2/sounds/pop.wav")];
+    this.sounds = [new Sound("pop", "./sprite2/sounds/pop.wav")];
 
     this.triggers = [
       new Trigger(Trigger.KEY_PRESSED, { key: "f" }, this.whenKeyFPressed),
