05963b92912bd0cc4e64443a6c52e8c6fe6284e6
Jairo7g <Jairo.gise07@gmail.com>
2025-07-25 10:27:04 -0600

    Add files via upload

---
+++ sprite2/Sprite2.js
@@ -0,0 +1,35 @@
+/* eslint-disable require-yield, eqeqeq */
+
+import {
+  Sprite,
+  Trigger,
+  Watcher,
+  Costume,
+  Color,
+  Sound,
+} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+export default class Sprite2 extends Sprite {
+  constructor(...args) {
+    super(...args);
+
+    this.costumes = [
+      new Costume("costume1", "./Sprite2/costumes/costume1.svg", {
+        x: -41.18799999999999,
+        y: -19.42641991674938,
+      }),
+      new Costume("costume2", "./Sprite2/costumes/costume2.svg", {
+        x: 13.68003631721001,
+        y: -19.426419833498784,
+      }),
+    ];
+
+    this.sounds = [new Sound("pop", "./Sprite2/sounds/pop.wav")];
+
+    this.triggers = [
+      new Trigger(Trigger.KEY_PRESSED, { key: "f" }, this.whenKeyFPressed),
+    ];
+  }
+
+  *whenKeyFPressed() {}
+}
